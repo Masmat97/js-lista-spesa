5b5e11c7e762aa9e6084793aa8b4c2a9b23d11e7
Matteo Mascio <matteo.mascio@libero.it>
2024-04-26 16:48:44 +0200

    M1-end

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const lista = [
+    "Pane",
+    "Latte",
+    "Biscotti",
+    "Pasta",
+    "Carne",
+    "Verdure",
+    "Frutta",
+]
+
+for (let i = 0; i < lista.length; i++) {
+    
+    const element = lista[i];
+    console.log(i, element)
+    
+}
